edec7f50ab52821f844394fcc572ef0b3ffc2537
Krisanu <kisanudey886@gmail.com>
2021-12-06 09:34:45 +0530

    click button change text color

---
+++ sample.js
@@ -0,0 +1,9 @@
+function middle1() {
+    document.getElementById("section-1").style.color = "white";
+}
+function middle2() {
+    document.getElementById('section-1').style.color = "black";
+}
+function middle3() {
+    document.getElementById('section-1').style.color = "red";
+}
